01ee58688602f44573f89fcac7b6ef40c8f8a88e
foudilredaoui <df_redaoui@esi.dz>
2018-03-02 20:24:48 +0100

    adding post method

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require("express");
 var app = express();
-app.set("view engine","ejs");
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set("view engine", "ejs");
+var friends = ["Tony", "Miranda", "Justin", "Lily"];
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render("home");
 });
-app.get("/friends",function(req,res){
- res.render("friends");
+app.post("/addfriend", function (req, res) {
+    var newfriend = req.body.newfriend;
+    friends.push(newfriend);
+    res.redirect("/friends");
 });
-app.listen(3000,function(){
-console.log("server started");
+app.get("/friends", function (req, res) {
+    res.render("friends", {
+        friends: friends
+    });
+});
+app.listen(3000, function () {
+    console.log("server started");
 });
